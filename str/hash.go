9170f0753624a1866c8964e4cbed53ea3f925b9c
cml <1554714484@qq.com>
2023-05-22 13:02:22 +0800

    feat: str.hash add function of hash

--- str/hash.go
+++ str/hash.go
@@ -1,9 +1,32 @@
 package str
 
-import "hash/fnv"
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"fmt"
+	"hash/fnv"
+)
 
-func HashStr(s string) uint32 {
+func HashCode(s string) uint32 {
 	f := fnv.New32a()
 	_, _ = f.Write([]byte(s))
 	return f.Sum32()
+}
+
+func Md5(s string) string {
+	m := md5.New()
+	_, _ = m.Write([]byte(s))
+	return fmt.Sprintf("%x", m.Sum(nil))
+}
+
+func Sha256(dst string) string {
+	h := sha256.New()
+	_, _ = h.Write([]byte(dst))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func Sha256WithSalt(dst string, salt string) string {
+	h := sha256.New()
+	_, _ = h.Write([]byte(dst + salt))
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
