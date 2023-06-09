bdb75c1315a6f880457a8d20f4c14b4144bf1a06
cml <1554714484@qq.com>
2023-06-09 11:07:21 +0800

    feat: add pkg of netx to parse IP with http

---
+++ netx/ip.go
@@ -0,0 +1,40 @@
+package netx
+
+import (
+	"encoding/binary"
+	"net"
+	"net/http"
+)
+
+const (
+	XForwardedFor = "X-Forwarded-For"
+	XRealIP       = "X-Real-IP"
+)
+
+// GetRemoteIp return client request's IP. example: 10.168.1.102
+func GetRemoteIp(req *http.Request) string {
+	remoteAddr := req.RemoteAddr
+	if ip := req.Header.Get(XRealIP); ip != "" {
+		remoteAddr = ip
+	} else if ip = req.Header.Get(XForwardedFor); ip != "" {
+		remoteAddr = ip
+	} else {
+		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
+	}
+
+	if remoteAddr == "::1" {
+		remoteAddr = "127.0.0.1"
+	}
+
+	return remoteAddr
+}
+
+// Ip2Uint32 Convert IPv4 string form to uint32
+func Ip2Uint32(ipStr string) uint32 {
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		return 0
+	}
+	ip = ip.To4()
+	return binary.BigEndian.Uint32(ip)
+}
