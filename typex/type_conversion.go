fd9dfeeca911f1e5d4789ad156659da2e7d760f6
cml <1554714484@qq.com>
2023-06-29 18:27:00 +0800

    feat: typex add AnyToStr()

--- typex/type_conversion.go
+++ typex/type_conversion.go
@@ -53,3 +53,35 @@ func AnyToInt(i interface{}) int {
 	}
 	return 0
 }
+
+func AnyToStr(v any) string {
+	switch x := v.(type) {
+	case string:
+		return x
+	case int:
+		return strconv.Itoa(x)
+	case int8:
+		return strconv.Itoa(int(x))
+	case int16:
+		return strconv.Itoa(int(x))
+	case int32:
+		return strconv.Itoa(int(x))
+	case int64:
+		return strconv.FormatInt(v.(int64), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(x), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(x), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(x), 10)
+	case uint64:
+		return strconv.FormatUint(x, 10)
+	case float64:
+		return strconv.FormatFloat(v.(float64), 'f', 6, 64)
+	case float32:
+		return strconv.FormatFloat(float64(v.(float32)), 'f', 6, 32)
+	case bool:
+		return strconv.FormatBool(v.(bool))
+	}
+	return ""
+}
